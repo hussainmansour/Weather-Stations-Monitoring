19640d0b8028959366425f0b292978544f1f4692
Abdulrahman-Aladdin <abdulrahmanalaaeldin1@gmail.com>
2025-05-24 11:49:11 +0300

    clean weather station code

--- weather-station/src/main/java/weather/station/WeatherStation.java
+++ weather-station/src/main/java/weather/station/WeatherStation.java
@@ -14,79 +14,77 @@ import java.util.Properties;
 import java.util.Random;
 
 public class WeatherStation {
-    private static final int noOfRecords = 10_000;
     private static final double lowBatteryPercentage = 0.3;
     private static final double mediumBatteryPercentage = 0.4;
+    private static final String topic = "weather-data";
 
     public static void main(String[] args) {
-        Properties props = new Properties();
-        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("BOOTSTRAP_SERVERS"));
-        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
-        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, System.getenv("SCHEMA_REGISTRY_URL"));
+        Properties props = getProps();
+        long stationId = Long.parseLong(System.getenv("STATION_ID"));
+        long seq = 0;
+        Random rand = new Random();
+        Schema schema = WeatherData.getClassSchema();
 
-        String topic = "weather-data";
-
-        int lowBatteryCount = (int) (noOfRecords * lowBatteryPercentage);
-        int mediumBatteryCount = (int) (noOfRecords * mediumBatteryPercentage);
-        int highBatteryCount = noOfRecords - lowBatteryCount - mediumBatteryCount;
+        System.out.println("Sending weather data to Kafka...");
 
         try (Producer<String, GenericRecord> producer = new KafkaProducer<>(props)) {
-            Random rand = new Random();
-            long stationId = 1L;
-            long seq = 0;
-
-            int loops = noOfRecords;
-
-            System.out.println("Sending weather data to Kafka...");
-
-            Schema schema = WeatherData.getClassSchema();
-
-            while (loops-- > 0) {
-                boolean validBattery = false;
-                int batteryStatus = rand.nextInt(3);
-                while (!validBattery) {
-                    if (batteryStatus == 0 && lowBatteryCount > 0) {
-                        lowBatteryCount--;
-                        validBattery = true;
-                    } else if (batteryStatus == 1 && mediumBatteryCount > 0) {
-                        mediumBatteryCount--;
-                        validBattery = true;
-                    } else if (batteryStatus == 2 && highBatteryCount > 0) {
-                        highBatteryCount--;
-                        validBattery = true;
-                    } else {
-                        batteryStatus = (batteryStatus + rand.nextInt(1, 3)) % 3;
-                    }
-                }
-
-
-                GenericRecord genericRecord = new GenericData.Record(schema);
-                genericRecord.put("station_id", stationId);
-                genericRecord.put("s_no", seq++);
-                genericRecord.put("battery_status", batteryStatus == 0 ? BatteryStatus.low : batteryStatus == 1 ?
-                        BatteryStatus.medium : BatteryStatus.high);
-                genericRecord.put("status_timestamp", System.currentTimeMillis());
-                genericRecord.put("weather", Weather.newBuilder().setHumidity(rand.nextInt()).setTemperature(rand.nextInt())
-                        .setWindSpeed(rand.nextInt()).build());
-
-                ProducerRecord<String, GenericRecord> record = new ProducerRecord<>(topic, String.valueOf(stationId), genericRecord);
-                boolean send = rand.nextInt(10) != 5;
-
-                if (send) {
-                    producer.send(record, (metadata, exception) -> {
-                        if (exception != null) {
-                            System.err.println("Failed to produce: " + exception.getMessage());
-                        }
-                    });
-                    System.out.println("Sent record: " + record);
-                }
+            while (true) {
+                GenericRecord status = generateStatusRecord(stationId, seq++, schema);
+                ProducerRecord<String, GenericRecord> record = new ProducerRecord<>(topic, String.valueOf(stationId), status);
+                sendStatus(producer, record, rand);
                 Thread.sleep(1000);
             }
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
+    }
 
-        System.out.println("Weather data sent to Kafka topic: " + topic);
+    private static void sendStatus(Producer<String, GenericRecord> producer, ProducerRecord<String, GenericRecord> record,
+                                   Random rand) {
+        if (rand.nextInt(10) != 5) {
+            producer.send(record, (metadata, exception) -> {
+                if (exception != null) {
+                    System.err.println("Failed to produce: " + exception.getMessage());
+                }
+            });
+            System.out.println("Sent record: " + record);
+        }
+    }
+
+    private static Properties getProps() {
+        Properties props = new Properties();
+        props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, System.getenv("BOOTSTRAP_SERVERS"));
+        props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, KafkaAvroSerializer.class);
+        props.put(KafkaAvroSerializerConfig.SCHEMA_REGISTRY_URL_CONFIG, System.getenv("SCHEMA_REGISTRY_URL"));
+        return props;
+    }
+
+    private static GenericRecord generateStatusRecord(long stationId, long seq, Schema schema) {
+        Random rand = new Random();
+        GenericRecord genericRecord = new GenericData.Record(schema);
+        genericRecord.put("station_id", stationId);
+        genericRecord.put("s_no", seq);
+        genericRecord.put("battery_status", getBatteryStatus(rand));
+        genericRecord.put("status_timestamp", System.currentTimeMillis());
+        genericRecord.put("weather", getWeather(rand));
+        return genericRecord;
+    }
+
+    private static BatteryStatus getBatteryStatus(Random rand) {
+        double randomValue = rand.nextDouble();
+        if (randomValue <= lowBatteryPercentage)
+            return BatteryStatus.low;
+        if (randomValue <= mediumBatteryPercentage)
+            return BatteryStatus.medium;
+        return BatteryStatus.high;
+    }
+
+    private static Weather getWeather(Random rand) {
+        return Weather.newBuilder()
+                .setHumidity(rand.nextInt(100))
+                .setTemperature(rand.nextInt(50))
+                .setWindSpeed(rand.nextInt(100))
+                .build();
     }
 }
